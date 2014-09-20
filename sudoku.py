1cc41ec1d3c8f25e3bcdb1cea696b227134e7d58
taylanbil <taylanbil@gmail.com>
2014-09-20 06:30:42 -0700

    changed some comments

--- sudoku.py
+++ sudoku.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 """
-bir grubun icinde iki kareden biri mutlaka 3 ise, ve o iki kare ayni sirada ise
-o siradaki diger 3 ihtimalleri elenmeli
+Sudoku solver... with explanations and everything...
 """
 
 
@@ -347,6 +346,17 @@ if __name__ == '__main__':
         [None, None, 5, None, 8, None, 6, None, None],
         [None, None, 7, None, None, None, 9, None, None],
     ]
+    extreme3 = [
+        [None, None, 5, None, None, None, 7, None, None],
+        [None, 9, None, 7, None, 1, None, 6, None],
+        [8, None, None, None, None, None, None, None, 5],
+        [None, 8, None, 9, None, 3, None, 2, None],
+        [None, None, None, None, 2, None, None, None, None],
+        [None, 7, None, 5, None, 6, None, 4, None],
+        [9, None, None, None, None, None, None, None, 1],
+        [None, 2, None, 6, None, 8, None, 9, None],
+        [None, None, 1, None, None, None, 4, None, None],
+    ]
 
-    S = get_sudoku_table(extreme2)
+    S = get_sudoku_table(extreme3)
     S.solve()
